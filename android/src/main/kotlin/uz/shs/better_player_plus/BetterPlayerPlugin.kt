dbb0320e9ddbbbf124d24ee38de1b976fd40e13f
Hudson Proenca <hproenca@mgmresorts.com>
2025-08-28 18:33:33 +0200

    chore: revert some changes

--- android/src/main/kotlin/uz/shs/better_player_plus/BetterPlayerPlugin.kt
+++ android/src/main/kotlin/uz/shs/better_player_plus/BetterPlayerPlugin.kt
@@ -82,10 +82,7 @@ class BetterPlayerPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
 
-    override fun onDetachedFromActivity() {
-        // Release disk cache to free file handles and memory-mapped resources when activity goes away
-        releaseCache()
-    }
+    override fun onDetachedFromActivity() {}
 
     private fun disposeAllPlayers() {
         for (i in 0 until videoPlayers.size()) {
